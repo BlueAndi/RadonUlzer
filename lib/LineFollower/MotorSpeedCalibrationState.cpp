1ba96c0eec2761c638bc6969215f441400f28b95
greyes <gabryelrdiaz@gmail.com>
2023-03-10 18:05:20 +0100

    removed unnecesary snprintf #1

--- lib/LineFollower/MotorSpeedCalibrationState.cpp
+++ lib/LineFollower/MotorSpeedCalibrationState.cpp
@@ -250,14 +250,8 @@ void MotorSpeedCalibrationState::phase4Finished(StateMachine& sm)
     }
     else
     {
-        char msg[Logging::MESSAGE_BUFFER_SIZE] = "";
-
-        snprintf(msg, Logging::MESSAGE_BUFFER_SIZE,
-                 "Calibrated max. speed: %d steps/s (= %d mm/s)",
-                 maxSpeed,
-                 (maxSpeed / RobotConstants::ENCODER_STEPS_PER_MM));
-
-        LOG_DEBUG("SpeedCal", msg);
+        LOG_DEBUG("SpeedCal", "Calibrated max. speed: %d steps/s (= %d mm/s)", maxSpeed,
+                  (maxSpeed / RobotConstants::ENCODER_STEPS_PER_MM));
 
         sm.setState(&LineSensorsCalibrationState::getInstance());
     }
