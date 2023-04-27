6c46ddfc583efe88c597459fd778874a7361733f
BlueAndi <web@blue-andi.de>
2023-04-27 19:43:52 +0200

    Convoy leader application added, which bases on the line follower application.

---
+++ lib/ConvoyLeader/App.cpp
@@ -0,0 +1,139 @@
+/* MIT License
+ *
+ * Copyright (c) 2023 Andreas Merkle <web@blue-andi.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*******************************************************************************
+    DESCRIPTION
+*******************************************************************************/
+/**
+ * @brief  LineFollower application
+ * @author Andreas Merkle <web@blue-andi.de>
+ */
+
+/******************************************************************************
+ * Includes
+ *****************************************************************************/
+#include "App.h"
+#include "StartupState.h"
+#include <Board.h>
+#include <Speedometer.h>
+#include <DifferentialDrive.h>
+#include <Odometry.h>
+#include <Util.h>
+
+/******************************************************************************
+ * Compiler Switches
+ *****************************************************************************/
+
+/******************************************************************************
+ * Macros
+ *****************************************************************************/
+
+/******************************************************************************
+ * Types and classes
+ *****************************************************************************/
+
+/******************************************************************************
+ * Prototypes
+ *****************************************************************************/
+
+/******************************************************************************
+ * Local Variables
+ *****************************************************************************/
+
+/* Name of Channel to send Position Data to. */
+const char* App::POSITION_CHANNEL = "POSITION";
+
+/******************************************************************************
+ * Public Methods
+ *****************************************************************************/
+
+void App::setup()
+{
+    Serial.begin(SERIAL_BAUDRATE);
+    Board::getInstance().init();
+    m_systemStateMachine.setState(&StartupState::getInstance());
+    m_controlInterval.start(DIFFERENTIAL_DRIVE_CONTROL_PERIOD);
+    m_yapServer.createChannel(POSITION_CHANNEL, POSITION_CHANNEL_DLC, positionCallback);
+}
+
+void App::loop()
+{
+    m_yapServer.process(millis());
+    Speedometer::getInstance().process();
+
+    if (true == m_controlInterval.isTimeout())
+    {
+        /* The differential drive control needs the measured speed of the
+         * left and right wheel. Therefore it shall be processed after
+         * the speedometer.
+         */
+        DifferentialDrive::getInstance().process(DIFFERENTIAL_DRIVE_CONTROL_PERIOD);
+
+        /* The odometry unit needs to detect motor speed changes to be able to
+         * calculate correct values. Therefore it shall be processed right after
+         * the differential drive control.
+         */
+        Odometry::getInstance().process();
+
+        /* Send Position to YAP Client */
+        reportPosition();
+
+        m_controlInterval.restart();
+    }
+
+    m_systemStateMachine.process();
+}
+
+/******************************************************************************
+ * Protected Methods
+ *****************************************************************************/
+
+/******************************************************************************
+ * Private Methods
+ *****************************************************************************/
+
+void App::reportPosition()
+{
+    int32_t xPos;
+    int32_t yPos;
+    uint8_t outBuf[POSITION_CHANNEL_DLC];
+
+    Odometry::getInstance().getPosition(xPos, yPos);
+
+    Util::int32ToByteArray(&outBuf[0U], (sizeof(outBuf) - sizeof(int32_t)), xPos);
+    Util::int32ToByteArray(&outBuf[4U], (sizeof(outBuf) - sizeof(int32_t)), yPos);
+
+    m_yapServer.sendData(POSITION_CHANNEL, outBuf, sizeof(outBuf));
+}
+
+void App::positionCallback(const uint8_t* payload, const uint8_t payloadSize)
+{
+}
+
+/******************************************************************************
+ * External Functions
+ *****************************************************************************/
+
+/******************************************************************************
+ * Local Functions
+ *****************************************************************************/
