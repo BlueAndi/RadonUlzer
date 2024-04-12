4885dfddc521616cac66b26c37b052e0e4ce1651
Andreas Merkle <andreas.merkle@newtec.de>
2024-04-12 14:50:28 +0200

    env["platform"] replaced with platform.system() to determine the platform.

--- scripts/webots_launcher.py
+++ scripts/webots_launcher.py
@@ -27,39 +27,40 @@
 ################################################################################
 import os
 import sys
+import platform
 
 Import("env") # pylint: disable=undefined-variable
 
 ################################################################################
 # Variables
 ################################################################################
-OS_WIN = "win32"
-OS_LINUX = "linux"
-OS_MACOS = "darwin"
-OS_TYPE = env["PLATFORM"] # pylint: disable=undefined-variable
+OS_PLATFORM_TYPE_WIN = "Windows"
+OS_PLATFORM_TYPE_LINUX = "Linux"
+OS_PLATFORM_TYPE_MACOS = "Darwin"
+OS_PLATFORM_TYPE = platform.system() # pylint: disable=undefined-variable
 PROGRAM_PATH = "$BUILD_DIR/"
 PROGRAM_OPTIONS_SOCKET = '-s'
 ROBOT_NAME = env.GetProjectOption("webots_robot_name") # pylint: disable=undefined-variable
 WEBOTS_CONTROLLER_OPTIONS = '--robot-name=' + ROBOT_NAME + ' --stdout-redirect'
 
-if OS_TYPE == OS_WIN:
+if OS_PLATFORM_TYPE == OS_PLATFORM_TYPE_WIN:
 
     WEBOTS_HOME = os.getenv('WEBOTS_HOME').replace('\\', '/')
     WEBOTS_CONTROLLER = '"' + WEBOTS_HOME + '/msys64/mingw64/bin/webots-controller.exe"'
     PROGRAM_NAME = "${PROGNAME}.exe"
 
-elif OS_TYPE == OS_LINUX:
+elif OS_PLATFORM_TYPE == OS_PLATFORM_TYPE_LINUX:
 
     WEBOTS_CONTROLLER = "$WEBOTS_HOME/webots-controller"
     PROGRAM_NAME = "${PROGNAME}"
 
-elif OS_TYPE == OS_MACOS:
+elif OS_PLATFORM_TYPE == OS_PLATFORM_TYPE_MACOS:
 
     WEBOTS_CONTROLLER = "$WEBOTS_HOME/Contents/MacOS/webots-controller"
     PROGRAM_NAME = "${PROGNAME}"
 
 else:
-    print(f"OS type {OS_TYPE} not supported.")
+    print(f"OS type {OS_PLATFORM_TYPE} not supported.")
     sys.exit(-1)
 
 WEBOTS_LAUNCHER_ACTION = WEBOTS_CONTROLLER + ' '\
