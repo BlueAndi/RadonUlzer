df195f2465a6702981df6bd00032a568ebd3d629
BlueAndi <web@blue-andi.de>
2024-04-17 23:15:48 +0200

    "//" replaced with "/* ... */".

--- lib/Service/Logging.h
+++ lib/Service/Logging.h
@@ -338,7 +338,7 @@ namespace Logging
         }
     }
 
-}; // namespace Logging
+}; /* namespace Logging */
 
 /******************************************************************************
  * Functions
