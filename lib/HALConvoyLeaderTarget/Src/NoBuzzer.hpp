626ea94a71a806a39d2ad63b03a52fa8c454dad4
Gabryel Reyes <gabryel.reyes@newtec.de>
2024-07-03 14:17:07 +0200

    Replace ConvoyLeaderTarget buzzer to save flash space

---
+++ lib/HALConvoyLeaderTarget/Src/NoBuzzer.hpp
@@ -0,0 +1,154 @@
+/* MIT License
+ *
+ * Copyright (c) 2023 - 2024 Andreas Merkle <web@blue-andi.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*******************************************************************************
+    DESCRIPTION
+*******************************************************************************/
+/**
+ * @brief  Buzzer Dummy
+ * @author Juliane Kerpe <juliane.kerpe@web.de>
+ *
+ * @addtogroup HALTarget
+ *
+ * @{
+ */
+#ifndef NOBUZZER_H
+#define NOBUZZER_H
+
+/******************************************************************************
+ * Compile Switches
+ *****************************************************************************/
+
+/******************************************************************************
+ * Includes
+ *****************************************************************************/
+
+#include <IBuzzer.h>
+
+/******************************************************************************
+ * Macros
+ *****************************************************************************/
+
+/******************************************************************************
+ * Types and Classes
+ *****************************************************************************/
+
+/** This class provides access to a Dummy Buzzer. */
+class NoBuzzer : public IBuzzer
+{
+public:
+    /**
+     * Constructs the buzzer adapter.
+     */
+    NoBuzzer() : IBuzzer()
+    {
+    }
+
+    /**
+     * Destroys the buzzer adapter.
+     */
+    ~NoBuzzer()
+    {
+    }
+
+    /**
+     * Plays the specified frequency for the specified duration.
+     *
+     * This function plays the note in the background while your program continues
+     * to execute. If you call another buzzer function while the note is playing,
+     * the new function call will overwrite the previous and take control of the
+     * buzzer.
+     *
+     * @warning @a frequency &times; @a duration / 1000 must be no greater than
+     * 0xFFFF (65535). This means you can't use a duration of 65535 ms for
+     * frequencies greater than 1 kHz. For example, the maximum duration you can
+     * use for a frequency of 10 kHz is 6553 ms. If you use a duration longer than
+     * this, you will produce an integer overflow that can result in unexpected
+     * behavior.
+     *
+     * @param[in] freq        Frequency to play in 0.1 Hz.
+     * @param[in] duration    Duration of the note in milliseconds.
+     * @param[in] volume      Volume of the note (0-15).
+     */
+    void playFrequency(uint16_t freq, uint16_t duration, uint8_t volume) final
+    {
+        /* Nothing to do. */
+        (void)freq;
+        (void)duration;
+        (void)volume;
+    }
+
+    /**
+     * Plays a melody sequence out of RAM.
+     * 
+     * @param[in] sequence Melody sequence in RAM
+     */
+    void playMelody(const char* sequence) final
+    {
+        /* Nothing to do. */
+        (void)sequence;
+    }
+
+    /**
+     * Plays a melody sequence out of program space.
+     * 
+     * @param[in] sequence Melody sequence in program space
+     */
+    void playMelodyPGM(const char* sequence) final
+    {
+        /* Nothing to do. */
+        (void)sequence;
+    }
+
+    /**
+     * Checks whether a note, frequency, or sequence is being played.
+     * 
+     * Note: Avoid calling this method inside a loop without processing the
+     * buzzer. On the simulation additional the simulation time needs to run!
+     *
+     * @return If the buzzer is current playing a note, frequency, or sequence it will
+     * return true otherwise false.
+     */
+    bool isPlaying() final
+    {
+        /* Nothing to do. */
+        return true;
+    }
+
+    /**
+     * Process the buzzer to handle sound timings.
+     */
+    void process() final
+    {
+        /* Nothing to do. */
+    }
+
+private:
+};
+
+/******************************************************************************
+ * Functions
+ *****************************************************************************/
+
+#endif /* NOBUZZER_H */
+/** @} */
