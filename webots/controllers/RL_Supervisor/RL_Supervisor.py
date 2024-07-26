afd6b14d6e71e27dc5847f1944be84020fd24276
Akram <Akram.Bzeouich@Newtec.de>
2024-07-26 15:21:30 +0200

    Rename variables and customize code formatting in the Supervisor class.

--- webots/controllers/RL_Supervisor/RL_Supervisor.py
+++ webots/controllers/RL_Supervisor/RL_Supervisor.py
@@ -1,20 +1,30 @@
-"""Supervisor controller.
-    Details: https://github.com/cyberbotics/webots/blob/master/docs/guide/supervisor-programming.md
 """
+Supervisor controller.
+
+Details: https://github.com/cyberbotics/webots/blob/master/docs/guide/supervisor-programming.md
+"""
+
 import sys
+import struct
+from controller import Supervisor
 from Serial_webots import SerialWebots
 from SerialMuxProt import Server
-from controller import Supervisor
-import struct
 
-# The PROTO DEF name must be given!
+# Constants
 ROBOT_NAME = "ROBOT"
-
-# The supervisor receiver name.
 SUPERVISOR_RX_NAME = "serialComRx"
-
-# The supervisor Emitter name.
 SUPERVISOR_TX_NAME = "serialComTx"
+COMMAND_CHANNEL_NAME = "CMD"
+CMD_DLC = 1
+SPEED_SETPOINT_CHANNEL_NAME = "SPEED_SET"
+SPEED_SET_DLC = 4
+LINE_SENSOR_CHANNEL_NAME = "LINE_SENS"
+STATUS_CHANNEL_NAME = "STATUS"
+MODE_CHANNEL_NAME = "MODE"
+CMD_ID_SET_READY_STATE = 1
+LINE_SENSOR_ON_TRACK_MIN_VALUE = 200
+POSITION_DATA = [-0.24713614078815466, -0.04863962992854465, 0.013994298332013683]
+ORIENTATION_DATA = [-1.0564747468923541e-06, 8.746699709178704e-07, 0.9999999999990595,  1.5880805820884731]
 
 # Create the Supervisor instance.
 supervisor = Supervisor()
@@ -25,12 +35,29 @@ timestep = int(supervisor.getBasicTimeStep())
 # Get the Supervisor Receiver Device
 supervisor_com_rx = supervisor.getDevice(SUPERVISOR_RX_NAME)
 
-# Get the Supervisor Emitter device 
-supervisor_com_Tx = supervisor.getDevice(SUPERVISOR_TX_NAME)
+# Get the Supervisor Emitter device
+supervisor_com_tx = supervisor.getDevice(SUPERVISOR_TX_NAME)
 
 # Set serial rx/tx channels for communication with the Roboter.
 supervisor_com_rx.setChannel(2)
-supervisor_com_Tx.setChannel(1)
+supervisor_com_tx.setChannel(1)
+
+# Get robot node which to observe.
+robot_node = supervisor.getFromDef(ROBOT_NAME)
+
+# SerialMuxProt Server Instance.
+s_client = SerialWebots(supervisor_com_tx, supervisor_com_rx)
+smp_server = Server(10, s_client)
+
+# Channel creation. 
+serialMuxProtChannelIdSPEED_SET = smp_server.create_channel(SPEED_SETPOINT_CHANNEL_NAME, SPEED_SET_DLC)
+serialMuxProtChannelIdCMD = smp_server.create_channel(COMMAND_CHANNEL_NAME, CMD_DLC)
+
+# Counter for the number of times no line has been detected
+no_line_detection_count = 0
+
+# The counter of how much it has been reset.
+reset_count = 0
 
 # Enable supervisor receiver to receive any message from the robot or other
 # devices in the simulation.
@@ -39,88 +66,67 @@ if supervisor_com_rx is None:
 else:
     supervisor_com_rx.enable(timestep)
 
-# Get robot node which to observe.
-robot_node = supervisor.getFromDef(ROBOT_NAME)
-
-# SerialMuxProt Server Instance.
-S_client = SerialWebots(supervisor_com_Tx, supervisor_com_rx)
-smp_server = Server(10, S_client) 
-
-#DLC of SPEED_SET Channel
-SPEED_SET_DLC = 4
-
-#DLC of STATUS Channel
-CMD_DLC = 1
-
-# Counter for the number of times no line has been detected
-noLineDetectionCount = 0
-
-# The line sensor threshold.
-Line_Sensor_On_Track_Min_Value = 200
-
-# The counter of how much it has been reset.
-Reset_Count = 0
-
 def reinitialize ():
-    """ Re-initialization of position and orientation of The ROBOT """
-    global robot_node,Reset_Count
+    """ Re-initialization of position and orientation of The ROBOT.
+    """
+    global reset_count
     trans_field = robot_node.getField("translation")
     rot_field = robot_node.getField("rotation")
-    intial_position =[-0.24713614078815466, -0.04863962992854465, 0.013994298332013683]
-    initial_orientation =[-1.0564747468923541e-06, 8.746699709178704e-07, 0.9999999999990595, 1.5880805820884731]
+    intial_position = POSITION_DATA
+    initial_orientation = ORIENTATION_DATA
     trans_field.setSFVec3f(intial_position)
     rot_field.setSFRotation(initial_orientation)
-    Reset_Count = 0
+    reset_count = 0
 
 def callback_Status(payload: bytearray) -> int:
-    """ Callback Status Channel """
+    """ Callback Status Channel.
+    """
     if payload[0] == 1:
-       print("the max. time within the robot must be finished its drive is  Done")
-       smp_server.send_data("SPEED_SET", struct.pack('2H', 0, 0))  # Stop motors
-       smp_server.send_data("CMD",struct.pack('B', 1))
-       reinitialize()
+        print("the max. time within the robot must be finished its drive is  Done")
+        smp_server.send_data(SPEED_SETPOINT_CHANNEL_NAME, struct.pack('2H', 0, 0))  # Stop motors
+        smp_server.send_data(COMMAND_CHANNEL_NAME,struct.pack('B', CMD_ID_SET_READY_STATE))   # SENDING A COMMAND_ID SET READY STATUS
+        reinitialize()       
 
 
 def callback_LineSensors(payload: bytearray)-> None:
-    """ Callback LINE_SENS Channel"""
-    global noLineDetectionCount, Line_Sensor_On_Track_Min_Value, Reset_Count
+    """ Callback LINE_SENS Channel.
+    """
+    global no_line_detection_count, reset_count
     sensor_data = struct.unpack('5H', payload)
+
     for idx in range (5):
-         if idx == 4:
-              print(f"Sensor[{idx}] = {sensor_data[idx]}")
-         else :
-               print(f"Sensor[{idx}] = {sensor_data[idx]}, ", end="")
+        if idx == 4:
+            print(f"Sensor[{idx}] = {sensor_data[idx]}")
+        else:
+            print(f"Sensor[{idx}] = {sensor_data[idx]}, ", end="")
                
     if all(value == 0 for value in sensor_data):
-        noLineDetectionCount += 1
+        no_line_detection_count += 1
     else:
-        noLineDetectionCount = 0
-    
-    if noLineDetectionCount >= 20 or all(value >= Line_Sensor_On_Track_Min_Value for value in sensor_data):
-        smp_server.send_data("SPEED_SET", struct.pack('2H', 0, 0))  # Stop motors, maximum NO Line Detection Counter reached
-        smp_server.send_data("CMD",struct.pack('B', 1))             # SENDING A COMMAND ID SET READY STATUS
-        Reset_Count += 1 
+        no_line_detection_count = 0
+
+    if no_line_detection_count >= 20 or all(value >= LINE_SENSOR_ON_TRACK_MIN_VALUE for value in sensor_data):
+
+        # Stop motors, maximum NO Line Detection Counter reached
+        smp_server.send_data(SPEED_SETPOINT_CHANNEL_NAME, struct.pack('2H', 0, 0)) 
+
+        # SENDING A COMMAND ID SET READY STATUS
+        smp_server.send_data(COMMAND_CHANNEL_NAME,struct.pack('B', CMD_ID_SET_READY_STATE))             
+        reset_count += 1
+
     else:
-        smp_server.send_data("SPEED_SET", struct.pack('2H', 1000, 1000)) 
-        
+        # Set left and right motors speed to 1000
+        smp_server.send_data(SPEED_SETPOINT_CHANNEL_NAME, struct.pack('2H', 1000, 1000))
     
 def callback_Mode(payload: bytearray)-> None:
-    """ Callback MODE Channel"""
+    """ Callback MODE Channel.
+    """
     driving_mode = payload[0]
+
     if driving_mode:
-        print("Driving Mode Selected")
+        print("Driving Mode Selected.")
     else:
-        print("Train Mode Selected")
-
-
-# Channel creation. 
-m_serialMuxProtChannelIdSPEED_SET = smp_server.create_channel("SPEED_SET", SPEED_SET_DLC)
-m_serialMuxProtChannelIdCMD = smp_server.create_channel("CMD", CMD_DLC)
-
-# Channel subscription.
-smp_server.subscribe_to_channel("STATUS", callback_Status)
-smp_server.subscribe_to_channel("LINE_SENS",callback_LineSensors)
-smp_server.subscribe_to_channel("MODE",callback_Mode)
+        print("Train Mode Selected.")
     
 def main_loop():
     """Main loop:
@@ -130,20 +136,28 @@ def main_loop():
         number: Status
     """
     status = 0
-    m_elapsedTimeSinceReset = 0
+    elapsedTimeSinceReset = 0  # Elapsed time since reset [ms]
 
-    if m_serialMuxProtChannelIdSPEED_SET == 0 or  m_serialMuxProtChannelIdCMD == 0 :
+    # Channel subscription.
+    smp_server.subscribe_to_channel(STATUS_CHANNEL_NAME, callback_Status)
+    smp_server.subscribe_to_channel(LINE_SENSOR_CHANNEL_NAME, callback_LineSensors)
+    smp_server.subscribe_to_channel(MODE_CHANNEL_NAME, callback_Mode)
+
+    if serialMuxProtChannelIdSPEED_SET == 0  or serialMuxProtChannelIdCMD == 0 :
         print("Channel SPEED_SET not created.")
     else:
+
         if robot_node is None:
             print(f"Robot DEF {ROBOT_NAME} not found.")
-            status = -1         
+            status = -1 
+
         else:
-             while supervisor.step(timestep) != -1:
-                   m_elapsedTimeSinceReset += timestep
-                   smp_server.process(m_elapsedTimeSinceReset)
-                   if Reset_Count != 0:
-                       reinitialize()                  
+            while supervisor.step(timestep) != -1:
+                elapsedTimeSinceReset += timestep
+                smp_server.process(elapsedTimeSinceReset)
+
+                if reset_count != 0:
+                    reinitialize()                  
     return status
 
 sys.exit(main_loop())
